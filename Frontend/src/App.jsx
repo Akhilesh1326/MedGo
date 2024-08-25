8ee38478343852f303ce4df5f4ac358740d9cd6d
Akhilesh26 <akhileshpimple3@gmail.com>
2024-08-26 00:25:46 +0530

    create new file for patient managment and added route to app file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,6 +13,7 @@ import DoctorValidation from './Doctor/Doctor_Validation';
 import DoctorDashboard from './Doctor/Doctor_Dashboard';
 import DoctorProfile from "./Doctor/Doctor_Profile";
 import ManageAppoinment from "./Doctor/Dashboard/ManageAppoinment";
+import PatientManagment from "./Doctor/Dashboard/PatientManagement"
 
 import CheckUser from './Components/CheckUser';
 import Home from './Components/Home';
@@ -26,12 +27,15 @@ const router = createBrowserRouter([
     { path: "/user/doctor/validation", element: <DoctorValidation /> },
     { path: "/user/doctor/dashboard", element: <DoctorDashboard /> },
     { path: "/user/doctor/profile", element: <DoctorProfile />},
-    { path: "user/doctor/dashboard/manage-appoinment", element: <ManageAppoinment />},
+    { path: "/user/doctor/dashboard/manage-appoinment", element: <ManageAppoinment />},
+    { path: "/user/doctor/dashboard/manage-patients", element: <PatientManagment />},
+
     { path: "/user/patient/signin", element: <PatientSignIn /> },
     { path: "/user/patient/login-credentials", element: <PatientLoginCred /> },
     { path: "/user/patient/personal-information", element: <PatientPI /> },
     { path: "/user/patient/validation", element: <PatientValidation /> },
     { path: "/user/patient/dashboard", element: <PatientDashboard /> },
+    
     { path: "/user/checkuser", element: <CheckUser /> },
     { path: "/user/commonarea", element: <CommonArea />},
 ]);
---
+++ Frontend/src/Doctor/Dashboard/PatientManagement.jsx
@@ -0,0 +1,103 @@
+import { Link } from "react-router-dom";
+import { useState } from 'react'
+import HeaderForDashboardComponent from "./HeaderForDashboardComponent"
+
+
+const PatientManagement = () => {
+    const [PatientEditPanel, setPatientEditPanel] = useState(false);
+    const [PatientHistoryPanel, setPatientHistoryPanel] = useState(false);
+
+    const handlePatientEditPanel = () => {
+        setPatientEditPanel(!PatientEditPanel);
+    }
+    const handlePatientHistoryPanel = () => {
+        setPatientHistoryPanel(!PatientHistoryPanel);
+    }
+
+    return (
+        <div className="bg-slate-800 text-white">
+            <HeaderForDashboardComponent />
+            <button className="border-2 border-black rounded-xl py-2 px-4 " onClick={handlePatientEditPanel}>Add new Patient</button>
+            <div className={`border-2 border-black rounded-xl w-fit scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100 z-50 absolute bg-white ${PatientEditPanel ? "" : "hidden"} `}>
+                <div>Personal Information</div>
+                <input type="text" placeholder="Full name" />
+                <input type="text" placeholder="DOB" />
+                <input type="text" placeholder="Gender" />
+                <div>Contact Information:</div>
+                <input type="text" placeholder="Contact Number" />
+                <input type="text" placeholder="Email if Available" />
+                <div>Address</div>
+                <input type="text" placeholder="complete Address" />
+                <div>Emergency Contact</div>
+                <input type="text" placeholder="Emeregency Contact" />
+                <input type="text" placeholder="Emeregency ContactName" />
+                <div>Medical Information</div>
+                <div>Medical History</div>
+                <input type="text" name="" id="" placeholder="Known Allergies (e.g., medications, foods, etc.)" />
+                <input type="text" name="" id="" placeholder="Chronic Conditions (e.g., diabetes, hypertension)" />
+                <input type="text" name="" id="" placeholder="Past Surgeries" />
+                <input type="text" name="" id="" placeholder="Family Medical History (e.g., hereditary diseases)" />
+                <div>Current Medications</div>
+                <input type="text" placeholder="List of medications currently being taken" />
+                <div>Insurance Information</div>
+                <input type="text" placeholder="Insurance Provider" />
+                <input type="text" placeholder="Policy Number" />
+                <div>Blood Type</div>
+                <input type="text" placeholder="blood type" />
+                <div>Smoking/Alcohol/Drug Use</div>
+                <input type="text" name="" id="" placeholder="Social history" />
+                <div>Height and Weight</div>
+                <input type="text" placeholder="For baseline measurements" />
+                <div>Optional but useful Information</div>
+                <input type="text" placeholder="Occupation" />
+                <input type="text" placeholder="Marital Status" />
+                <input type="text" placeholder="Preferred Pharmacy" />
+            </div>
+            <div>Patient History</div>
+            <div className="flex flex-col border-2 border-black rounded-lg w-fit items-center py-0 px-4">
+                <img src="" alt="img" className="h-16 w-16 border-2 border-black rounded-full mt-2" />
+                <div>Patient Name</div>
+                <div>contact</div>
+                <div>age</div>
+                <button className={`bg-slate-800 text-white border-2 border-black rounded-full `} onClick={handlePatientHistoryPanel}>Check Patient</button>
+                <div className={`bg-slate-700 absolute z-40 flex flex-col border-2 border-black rounded-lg ml-[400px] w-[450px] ${PatientHistoryPanel ? "" : "hidden"}`}>
+                    <div>Full Name</div>
+                    <div>Date of Birth (with Age calculation)</div>
+                    <div>Gender</div>
+                    <div>Contact Information (Phone, Email)</div>
+                    <div>Address</div>
+                    <div>Emergency Contact Information</div>
+                    <div className="text-xl">Medical History Section</div>
+                    <div>Allergies: List of known allergies with severity levels if applicable</div>
+                    <div>Chronic Conditions: Detailed list of chronic conditions (e.g., diabetes, hypertension)</div>
+                    <div>Past Surgeries: Description and dates of any surgeries</div>
+                    <div>Family Medical History: Information on hereditary conditions</div>
+                    <div className="text-xl">Ongoing Treatments</div>
+                    <div>Current Medications</div>
+                    <div>Treatment Plans</div>
+                    <div>Assigned Healthcare Providers</div>
+                    <div className="text-xl">Appointment Records</div>
+                    <div>Upcoming Appointments</div>
+                    <div>Past Appointments</div>
+                    <div className="text-xl"> Medical Documents</div>
+                    <div>Lab Results</div>
+                    <div>Imaging Results</div>
+                    <div>Discharge Summaries</div>
+                    <div>Referral Letters:</div>
+                    <div className="text-xl"> Insurance and Billing</div>
+                    <div>Insurance Information</div>
+                    <div>Billing History</div>
+                    <div className="text-xl"> Insurance and Billing</div>
+
+                    <button className="bg-slate-800 text-white border-2 border-black rounded-full w-fit" onClick={handlePatientHistoryPanel}>Exit Details</button>
+                    <button className="bg-slate-800 text-white border-2 border-black rounded-full w-fit">Edit Details</button>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default PatientManagement
+
