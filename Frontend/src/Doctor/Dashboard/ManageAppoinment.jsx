8706b0f19fa351b1cdc3f305511fde6c0d3357b1
Akhilesh26 <akhileshpimple3@gmail.com>
2024-08-26 00:27:44 +0530

    card remove

--- Frontend/src/Doctor/Dashboard/ManageAppoinment.jsx
+++ Frontend/src/Doctor/Dashboard/ManageAppoinment.jsx
@@ -1,6 +1,9 @@
 import {useState} from 'react'
 import {io} from "socket.io-client";
 import { v4 as uuid } from 'uuid';
+import {Link} from "react-router-dom";
+
+import HeaderForDashboardComponent from "./HeaderForDashboardComponent"
 
 const ManageAppoinment = () => {
   const [sendMsg, setSendMsg] = useState("");
@@ -32,17 +35,11 @@ const ManageAppoinment = () => {
   }
   return (
     <div>
-      <header className='w-full border-2 border-black flex justify-between h-16'>
-        <span>Logo</span>
-        <div>
-        <span className='ml-10'>button1</span>
-        <span className='ml-10'>button2</span>
-        </div>
-      </header>
+      <HeaderForDashboardComponent/>
       <button className='border-2 border-blue-700 rounded-xl' onClick={handleAppointmentPop}>
   {appointmentPop ? "Make Appointment" : "Exit Edit"}</button>
 
-<div className={` border-4 border-blue-800 rounded-xl w-[60%] ${appointmentPop ? "hidden" : "flex flex-col"} transition-all duration-300`}>
+<div className={`z-10 absolute bg-blue-900 border-4 border-blue-800 rounded-xl w-[60%] ${appointmentPop ? "hidden" : "flex flex-col"} transition-all duration-300`}>
   <input 
     type="text" 
     placeholder='Title' 
@@ -71,17 +68,78 @@ const ManageAppoinment = () => {
     Send Message
   </button>
 </div>
+<div className='grid grid-cols-2'>
 
-<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl'>
-  {/* <img src='' alt='doct' className='bg-slate-700 rounded-full py-1 px-2'> */}
-  <img src="no" alt="img" className='bg-slate-700 rounded-full   w-16 h-16 self-center'/>
-  <label className='' htmlFor="Doctor name">name of the doctor</label>
+<div className='grid grid-cols-3'>
+<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl h-fit'>
+  <label className='' htmlFor="">title</label>
   <label htmlFor="review">timing</label>
-  <label className='' htmlFor="description">date</label>
-  <label className='' htmlFor="description">review</label>
-  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>book an appoinment</button>
+  <label className='' htmlFor="description">creation date</label>
+  <label className='' htmlFor="description"></label>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Booking Status</button>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Edit appoinment</button>
 </div>
 
+<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl'>
+  <label className='' htmlFor="">title</label>
+  <label htmlFor="review">timing</label>
+  <label className='' htmlFor="description">creation date</label>
+  <label className='' htmlFor="description"></label>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Booking Status</button>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Edit appoinment</button>
+</div>
+<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl'>
+  <label className='' htmlFor="">title</label>
+  <label htmlFor="review">timing</label>
+  <label className='' htmlFor="description">creation date</label>
+  <label className='' htmlFor="description"></label>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Booking Status</button>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Edit appoinment</button>
+</div>
+<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl'>
+  <label className='' htmlFor="">title</label>
+  <label htmlFor="review">timing</label>
+  <label className='' htmlFor="description">creation date</label>
+  <label className='' htmlFor="description"></label>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Booking Status</button>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Edit appoinment</button>
+</div>
+</div>
+<div className='grid grid-cols-3'>
+<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl h-fit'>
+  <label className='' htmlFor="">title</label>
+  <label htmlFor="review">timing</label>
+  <label className='' htmlFor="description">creation date</label>
+  <label className='' htmlFor="description"></label>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Booking Status</button>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Edit appoinment</button>
+</div>
+<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl h-fit'>
+  <label className='' htmlFor="">title</label>
+  <label htmlFor="review">timing</label>
+  <label className='' htmlFor="description">creation date</label>
+  <label className='' htmlFor="description"></label>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Booking Status</button>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Edit appoinment</button>
+</div>
+<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl h-fit'>
+  <label className='' htmlFor="">title</label>
+  <label htmlFor="review">timing</label>
+  <label className='' htmlFor="description">creation date</label>
+  <label className='' htmlFor="description"></label>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Booking Status</button>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Edit appoinment</button>
+</div>
+<div className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl h-fit'>
+  <label className='' htmlFor="">title</label>
+  <label htmlFor="review">timing</label>
+  <label className='' htmlFor="description">creation date</label>
+  <label className='' htmlFor="description"></label>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Booking Status</button>
+  <button className='border-2 border-black bg-slate-900 rounded-full text-white py-1 px-2'>Edit appoinment</button>
+</div>
+</div>
+</div>
     </div>
   )
 }
