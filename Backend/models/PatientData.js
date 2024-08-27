cd0243586e68f6d1c0cb574d16bf41e601e0ca6f
Akhilesh26 <akhileshpimple3@gmail.com>
2024-08-27 15:01:53 +0530

    create patient sign in and up information db models

--- Backend/models/PatientData.js
+++ Backend/models/PatientData.js
@@ -19,3 +19,60 @@ const PatientBasic = new mongoose.Schema({
     },
 },{timestamps:true});
 
+const patientBasic  = mongoose.model("patientUserBasic", PatientBasic);
+
+const PatientMedicalInformation = new mongoose.Schema({
+    currentMedications:{
+        type: String,
+        
+    },
+    knowAllergies:{
+        type: String,
+    },
+    medicalHistory:{
+        type: String,
+    },
+    insuranceInformation:{
+        type: String,
+    },
+}, {timestamps: true});
+
+const patientMedicalInformation = mongoose.model("patientUserMedicalInformation", PatientMedicalInformation);
+
+const PatientIdentification = new mongoose.Schema({
+    governmentIssuedId: {
+        type: Image,
+        required: true,
+    },
+    adharNumber: {
+        type: Number,
+        required: true,
+    }
+}, {timestamps: true});
+
+const patientIdentification = mongoose.model("patientUserIdentification", PatientIdentification);
+
+const PatientLoginCredentials = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    
+}, {timestamps:true});
+
+const patientloginCredentials = mongoose.model("patientUserLoginCredentials", PatientLoginCredentials);
+
+module.exports ={
+    patientBasic,
+    patientIdentification,
+    patientMedicalInformation,
+    patientloginCredentials,
+};
