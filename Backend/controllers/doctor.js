75f44940371da68ffa943442dd2905f4048dc1f6
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-10 10:32:15 +0530

    commenete console log in handleLoginUserWithId, add parameter for doctorid for other information of doctor and add function as handleDoctorIdentificationInfo

--- Backend/controllers/doctor.js
+++ Backend/controllers/doctor.js
@@ -8,17 +8,30 @@ const {
 
 // Helper function to standardize responses
 // const createResponse = (DBmsg) => ({ DBmsg });
-async function handleLoginUser({userName, email, password}){
-    try{
+async function handleLoginUser({ userName, email, password }) {
+    try {
         const result = await doctorloginInfo.findOne({ userName: userName });
         // console.log("result - ", result);
-        if(result === null){
-            return {userName:"NotFound"}
+        if (result === null) {
+            return { userName: "NotFound" }
         }
         return result;
-    } catch(err){
+    } catch (err) {
         console.log("Error while validating login in controler", err.messsage);
-        return {userName:"NotFound"}
+        return { userName: "NotFound" }
+    }
+}
+async function handleLoginUserWithId(id) {
+    try {
+        const result = await doctorloginInfo.findById(id);
+        // console.log("result - ", result);
+        if (result === null) {
+            return { userName: "NotFound" }
+        }
+        return result;
+    } catch (err) {
+        console.log("Error while validating login in controler", err.messsage);
+        return { userName: "NotFound" }
     }
 }
 
@@ -30,19 +43,40 @@ async function handleDoctorLoginInfo(userName, email, password) {
 
 }
 
-async function handleDoctorAgreementInfo(termsAndServices, privacyAndPolicy, consentOfDataSharing) {
-    const result = await doctorAgreementInfo.create({ termsAndServices, privacyAndPolicy, consentOfDataSharing });
+async function handleDoctorAgreementInfo(DoctorLogInId, termsAndServices, privacyAndPolicy, consentOfDataSharing) {
+    const result = await doctorAgreementInfo.create({ DoctorLogInId, termsAndServices, privacyAndPolicy, consentOfDataSharing });
     return result;
 }
 
-async function handleDoctorBasicInfo(fullName, phone, dateOfBirth, gender) {
-    const result = await doctorBasicInfo.create({ fullName, phone, dateOfBirth, gender });
+async function handleDoctorBasicInfo(DoctorLogInId, fullName, phone, dateOfBirth, gender) {
+    const result = await doctorBasicInfo.create({ DoctorLogInId, fullName, phone, dateOfBirth, gender });
+    return result;
+}
+async function handleDoctorIdentificationInfo(DoctorLogInId, proofOfMedicalLicense, proofOfIdentity, Certifications) {
+    const result = await doctorIdentificationInfo.create({ DoctorLogInId, proofOfMedicalLicense, proofOfIdentity, Certifications });
     return result;
 }
 
-async function handleDoctorProfessionalInfo(medicalLicenseNumber, specialization, yearsOfExperience, medicalSchool, hospitalOrClinic) {
-    const result = await doctorProfessionalInfo.create({ medicalLicenseNumber, specialization, yearsOfExperience, medicalSchool, hospitalOrClinic});
+async function handleDoctorProfessionalInfo(DoctorLogInId, medicalLicenseNumber, specialization, yearsOfExperience, medicalSchool, hospitalOrClinic) {
+    const result = await doctorProfessionalInfo.create({ DoctorLogInId, medicalLicenseNumber, specialization, yearsOfExperience, medicalSchool, hospitalOrClinic });
     return result;
+}
+
+async function handleDoctorAllProfileData(id) {
+    try {
+        const result1 = await doctorloginInfo.findById(id).lean();
+        const result2 = await doctorBasicInfo.findOne({ DoctorLogInId: id }).lean();
+        const result3 = await doctorAgreementInfo.findOne({ DoctorLogInId: id }).lean();
+        const result4 = await doctorProfessionalInfo.findOne({ DoctorLogInId: id }).lean();
+
+        const result = { ...result1, ...result2, ...result3, ...result4 };
+        console.log(result);
+
+        return result;
+    } catch (error) {
+        console.log("Error at gettin all doctor data at controller = ", error)
+    }
+    // const result = await .findOne    
 }
 
 module.exports = {
@@ -51,4 +85,7 @@ module.exports = {
     handleDoctorBasicInfo,
     handleDoctorProfessionalInfo,
     handleLoginUser,
+    handleLoginUserWithId,
+    handleDoctorAllProfileData,
+    handleDoctorIdentificationInfo,
 };
