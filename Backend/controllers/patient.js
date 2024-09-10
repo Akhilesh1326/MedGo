554ed3b7fa84147d8d408eceaefde0f6d3f60cfe
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-10 10:33:50 +0530

    add function for getting all patient data, add parameter as pateintId for other information to connect with login info, add function handleLoginPatientUser to login in patient

--- Backend/controllers/patient.js
+++ Backend/controllers/patient.js
@@ -1,20 +1,23 @@
 const {patientBasic, 
     patientIdentification, 
     patientMedicalInformation,
-    patientloginCredentials} = require("../models/PatientData")
+    patientloginCredentials,
+    patientPersonalInformation} = require("../models/PatientData")
 
-async function handlePatientBasic(fullName, phone, dateOfBirth, gender) {
-    const result = await patientBasic.create({fullName, phone, dateOfBirth, gender});
+async function handlePatientBasic(patientIdOfLogCred,fullName, phone, dateOfBirth, gender) {
+    console.log("good",patientIdOfLogCred);
+    const result = await patientBasic.create({patientIdOfLogCred,fullName, phone, dateOfBirth, gender});
     return result;
 }
 
-async function handlePatientIdentification(governmentIssuedId, adharNumber){
-    const result = await patientIdentification.create({governmentIssuedId, adharNumber});
+async function handlePatientIdentification(patientIdOfLogCred,governmentIssuedId, adharNumber){
+    const result = await patientIdentification.create({patientIdOfLogCred,governmentIssuedId, adharNumber});
     return result;
 }
 
-async function handlePatientMedicalInformation(currentMedications, knowAllergies,medicalHistory, insuranceInformation) {
-    const result = await patientMedicalInformation.create({currentMedications, knowAllergies, medicalHistory, insuranceInformation});
+async function handlePatientMedicalInformation(patientIdOfLogCred,currentMedications, knowAllergies,medicalHistory, pastSurgeries, familyMedicalHistory) {
+    // console.log(patientIdOfLogCred)
+    const result = await patientMedicalInformation.create({patientIdOfLogCred,currentMedications, knowAllergies, medicalHistory,  pastSurgeries, familyMedicalHistory});
     return result
 };
 
@@ -23,9 +26,54 @@ async function handlePatientLoginCredentials(username, email, password){
     return result;
 };
 
+async function handlePatientPersonalInformation(patientIdOfLogCred, martialStatus, occupation, languagePreference){
+    const result = await patientPersonalInformation.create({patientIdOfLogCred, martialStatus, occupation, languagePreference});
+    return result;
+}
+
+async function handleLoginPatientUser(data) {
+    try {
+        const userName = data.userName;
+        console.log(userName)
+        const result = await patientloginCredentials.findOne({ username: userName });
+        console.log("result - ", result);
+        if (result === null) {
+            return { userName: "NotFound" }
+        }
+        
+        return result;
+    } catch (err) {
+        console.log("Error while validating login in controler", err.messsage);
+        return { userName: "NotFound" }
+    }
+}
+
+async function handleAllPatientProfileData(id) {
+    try {
+        console.log('id',id);
+        const result1 = await  patientBasic.findOne({patientIdOfLogCred:id})
+        const result2 = await  patientloginCredentials.findById(id)
+        const result3 = await  patientPersonalInformation.findOne({patientIdOfLogCred:id})
+        const result4 = await  patientMedicalInformation.findOne({patientIdOfLogCred:id})
+        const result5 = await  patientIdentification.findOne({patientIdOfLogCred:id})
+        console.log("result1 = ",result1)
+        console.log("result2 = ",result2)
+        console.log("result3 = ",result3)
+        console.log("result4 = ",result4)
+        console.log("result5 = ",result5)
+        const totalData = {...result1,...result2,...result3,...result4,...result5}
+        // console.log("totalData = ",totalData)
+    } catch (error) {
+        
+    }
+}
+
 module.exports = {
     handlePatientBasic,
     handlePatientIdentification,
     handlePatientLoginCredentials,
-    handlePatientMedicalInformation
+    handlePatientMedicalInformation,
+    handlePatientPersonalInformation,
+    handleAllPatientProfileData,
+    handleLoginPatientUser,
 };
