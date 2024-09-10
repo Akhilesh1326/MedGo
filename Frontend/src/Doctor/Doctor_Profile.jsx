76187bacaab38d7631d9655289f7f48de310507e
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-10 10:56:17 +0530

    add api call for getting all data

--- Frontend/src/Doctor/Doctor_Profile.jsx
+++ Frontend/src/Doctor/Doctor_Profile.jsx
@@ -1,6 +1,22 @@
 // import React from 'react'
+import axios from "axios"
+import { useEffect } from "react"
+
 
 const Doctor_Profile = () => {
+  
+
+  useEffect(()=>{
+    async function handleDoctorAllProfileData() {
+      try {
+        const resp = await axios.get("/api/user/doctor/all-profile-data")
+        console.log(resp);
+      } catch (error) {
+        console.log("Error while getting doctor profile data ",error)
+      }
+    }
+    handleDoctorAllProfileData();
+  },[])
   return (
     <div>
       
