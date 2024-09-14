cafbd41f15410cf3991b1854fe2017aa03ca5cfa
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-14 22:41:37 +0530

    header component for patient dashboard

---
+++ Frontend/src/Patient/Dashboard/HeaderForDashboardComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const HeaderForDashboardComponent = () => {
+  return (
+    <div>
+      <header className='w-full border-2 border-black flex justify-between h-16'>
+        <span>Logo</span>
+        <div>
+        <span className='ml-10'>button1</span>
+        <Link to="/user/patient/dashboard"><button className='ml-10'>Dashboard</button></Link>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default HeaderForDashboardComponent
