2057194cb13f8804faeb4fda40fd0fcb87b1029f
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-14 22:42:14 +0530

    schema file for prescription

---
+++ Backend/models/Prescription.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const prescription  = new mongoose.Schema({
+    doctorId:{
+        type:String,
+        required:true,
+    },
+    patientId:{
+        type:String,
+        required:true,
+    },
+    appointmentId:{
+        type:String,
+        required:true,
+    },
+    medication:{
+        type:String,
+        required:true,
+    },
+    diagnosis:{
+        type:String,
+        required:true,
+    },
+    instructions:{
+        type:String,
+        required:true,
+    },
+}, {timestamps:true});
+
+const Prescription = mongoose.model("prescriptionSchema",prescription);
+
+module.exports={Prescription}
