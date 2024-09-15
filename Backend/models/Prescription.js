84eeccf677442c68f279043337f00df1403edc93
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-15 23:03:43 +0530

    create schema for offlinePatientPrescription

--- Backend/models/Prescription.js
+++ Backend/models/Prescription.js
@@ -29,4 +29,30 @@ const prescription  = new mongoose.Schema({
 
 const Prescription = mongoose.model("prescriptionSchema",prescription);
 
-module.exports={Prescription}
+const offlinePatientPrescription = new mongoose.Schema({
+    doctorId:{
+        type:String,
+        required:true,
+    },
+    appointmentId:{
+        type:String,
+        required:true,
+    },
+    medication:{
+        type:String,
+        required:true,
+    },
+    diagnosis:{
+        type:String,
+        required:true,
+    },
+    instructions:{
+        type:String,
+        required:true,
+    },
+},{timestamps:true})
+
+const OffilnePatientPrescription = mongoose.model("offlinePatientPrescriptionSchema",offlinePatientPrescription);
+
+
+module.exports={Prescription, OffilnePatientPrescription}
