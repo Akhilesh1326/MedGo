2badc681b3733b73d3f81f742d591c07e82f32bc
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-23 22:16:11 +0530

    add style for dashboard button

--- Frontend/src/Doctor/Dashboard/HeaderForDashboardComponent.jsx
+++ Frontend/src/Doctor/Dashboard/HeaderForDashboardComponent.jsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom"
 const HeaderForDashboardComponent = () => {
   return (
     <div>
-      <header className='w-full border-b-2 border-[#174876] flex justify-between h-16 bg-[#96BADC]'>
+      <header className='w-full border-b-2 border-[#174876] flex justify-between  bg-[#96BADC] px-5 py-4'>
         <span>Logo</span>
         <div>
-        <span className='ml-10'>button1</span>
-        <Link to="/user/doctor/dashboard"><button className='ml-10'>Dashboard</button></Link>
+        <Link to="/user/doctor/dashboard"><button className='bg-[#1c5081] py-2 px-2 rounded-md font-thin hover:shadow-md hover:shadow-[#3794eb] duration-300 hover:-translate-y-1'>Dashboard</button></Link>
         </div>
       </header>
     </div>
