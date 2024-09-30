eb6232d6d7162a9d7adc38eaac93c725612c4b68
Akhilesh26 <akhileshpimple3@gmail.com>
2024-09-30 22:38:51 +0530

    change in response logic

--- Frontend/src/Components/CommonArea.jsx
+++ Frontend/src/Components/CommonArea.jsx
@@ -9,6 +9,8 @@ const MyComponent = () => {
     const [appointmentData, setAppointmentData] = useState([]);
     const [socketMsg, setSocketMsg] = useState("");
     const [selectedAppointment, setSelectedAppointment] = useState(null);
+    const [appointmentPostResp, setAppointmentPostResp] = useState("")
+    const [loadAppointmentBook, setLoadAppointmentBook] = useState(false);
 
     const [bookStateMessage, setBookStateMessage] = useState("Book An Appointment")
 
@@ -47,15 +49,20 @@ const MyComponent = () => {
         const socket = io("http://localhost:8000");
 
         socket.emit('common-area-message', msg);
-        console.log("Message Sent");
+        // console.log("Message Sent");
 
-        console.log("appointment web-socket has sent")
+        // console.log("appointment web-socket has sent")
         try {
             const resp = await axios.post("/api/user/commonArea/book-appointment", {
                 appointmentId,
                 doctorId,
             });
-            console.log("AID = ", resp);
+            console.log("Hello = ", resp);
+            if(resp.data.msg == "Booked"){
+                setLoadAppointmentBook(false)
+                setAppointmentPostResp("Booked");
+
+            }
         } catch (error) {
             console.log("Error occured while send appointment id = ", error);
         }
@@ -75,7 +82,7 @@ const MyComponent = () => {
         <div>
             <div className="flex justify-between py-4 px-2 bg-slate-700 text-white">
                 <div className="flex">
-                    <div className="py-2 px-4 mx-1 my-2 border-slate-600 border-2 rounded-lg">Logo</div>
+                    <div className="py-2 px-4 mx-1 my-2 border-slate-600 border-2 rounded-lg font-raleway">Medlinea</div>
                     <input type="text" placeholder="Search appointments" className="py-2 px-2 mx-1 my-2 border-slate-600 border-2 rounded-lg" />
                 </div>
                 <div className="flex">
@@ -83,8 +90,8 @@ const MyComponent = () => {
                     <div className="py-2 px-4 mx-1 my-2 border-slate-600 border-2 rounded-lg">Dashboard</div>
                 </div>
             </div>
-            <div className="flex flex-col justify-center items-center z-0 h-screen" style={{ backgroundImage: `url(${bgOne})`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "repeat" }}>
-                <h1 className="font-light text-2xl mx-10 mt-20 mb-10">Appointments</h1>
+            <div className="flex flex-col justify-center items-center z-0 h-screen bg-slate-900">
+                <h1 className="font-light text-2xl mx-10 mt-20 mb-10 text-slate-200">Appointments</h1>
                 <div className="grid grid-cols-4 gap-7">
                     {appointmentData.map((item) => (
                         <div key={item._id} className='w-fit flex flex-col text-center border-2 border-blue-900 py-2 px-4 rounded-xl bg-white bg-opacity-80 shadow-md'>
@@ -120,7 +127,23 @@ const MyComponent = () => {
                             <div className="mb-2"><strong>Location Of Appointment:</strong> {selectedAppointment.locationOfAppointment}</div>
                             <div className="mb-2"><strong>Duration for Appointment:</strong> {selectedAppointment.durationOfAppointment}</div>
                         </div>
-                        <button className="mt-4 bg-blue-600 text-white py-2 px-4 rounded-lg" onClick={() => handleBookingOfAppointment(selectedAppointment._id, selectedAppointment.doctorId)}>Book Your Appointment</button>
+                        <button 
+  className={`mt-4 bg-blue-600 text-white py-2 px-4 rounded-lg ${loadAppointmentBook ? "animate-spin" : "animate-none"} ${appointmentPostResp === "Booked" ? "bg-green-500" : ""} `} 
+  onClick={() => {
+    handleBookingOfAppointment(selectedAppointment._id, selectedAppointment.doctorId);
+    setLoadAppointmentBook(true);
+  }} 
+  disabled={appointmentPostResp === "Booked"}
+>
+  { 
+    appointmentPostResp === "Booked" 
+      ? "Booking Done" 
+      : loadAppointmentBook 
+        ? "/" 
+        : "Book Your Appointment" 
+  }
+</button>
+
                     </div>
                 </div>
             )}
