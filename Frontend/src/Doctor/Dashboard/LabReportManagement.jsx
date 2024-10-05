8d55763911e69221729e3023d332b654e8c2a5fa
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-05 10:44:04 +0530

    create new component LabReportManagment for managing the patients lab reports

---
+++ Frontend/src/Doctor/Dashboard/LabReportManagement.jsx
@@ -0,0 +1,13 @@
+import Header from './HeaderForDashboardComponent';
+
+
+
+const LabReportManagement = () =>{
+    return(
+        <>
+        <Header />
+        </>
+    )
+} 
+
+export default LabReportManagement;
