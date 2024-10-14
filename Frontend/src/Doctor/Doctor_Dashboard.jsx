d4f672965c745c25f4cacbd3de5a1f83715d16b1
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-14 22:53:57 +0530

    remove unwanted comments

--- Frontend/src/Doctor/Doctor_Dashboard.jsx
+++ Frontend/src/Doctor/Doctor_Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import {io} from "socket.io-client";
 import { v4 as uuid } from 'uuid';
 import { Link } from "react-router-dom";
+// 96BADC
 
 const Doctor_Dashboard = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -46,18 +47,15 @@ const Doctor_Dashboard = () => {
 
   return (
     <div className="relative grid grid-cols-1 md:grid-cols-[300px_1fr] lg:grid-cols-[300px_1fr] xl:grid-cols-[300px_1fr] grid-rows-[80px_1fr] gap-2 p-4 bg-[#edf1f6] h-screen">
-      {/* Sidebar */}
+
       <div
         className={`fixed top-0 left-0 h-fit min-h-full bg-[#262c9c] text-white p-4 rounded-xl transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
           } transition-transform duration-300 md:relative md:translate-x-0 sm:row-span-2 md:row-span-2 lg:row-span-2 xl:row-span-2 sm:w-[300px] w-[250px]`}
-        // style={{ width: "300px" }}
       >
         <div className="mb-4 flex justify-between items-center">
           <div className="border-2 border-black my-2 mx-4 px-4 py-2">Logo</div>
-          {/* Close button for mobile */}
-          <button className="md:hidden text-white" onClick={toggleSidebar}>
-            &times; {/* Close Icon */}
-          </button>
+
+          <button className="md:hidden text-white" onClick={toggleSidebar}>&times;</button>
         </div>
         <Link to="/user/doctor/dashboard/manage-appoinment"><div className="border-2 border-black my-2 mx-4 py-2 px-5 rounded-lg font-bold bg-[#EDE9E3] text-slate-800 hover:bg-[#d0e3ff]">Appointment Management</div></Link>
         <Link to="/user/doctor/dashboard/manage-patients"><div className="border-2 border-black my-2 mx-4 py-2 px-5 rounded-lg font-bold bg-[#EDE9E3] text-slate-800 hover:bg-[#d0e3ff]">Patient Management</div></Link>
