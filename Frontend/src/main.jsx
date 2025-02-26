0f896d5efa34af3ecb0b443c89520e1180de4b4b
Akhilesh26 <akhileshpimple3@gmail.com>
2025-02-26 22:05:51 +0530

    import lang based library

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+import i18n from "./i18nDoctorDash.js"; // Import i18n
+import { I18nextProvider } from "react-i18next";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <I18nextProvider i18n={i18n}>
     <App />
+  </I18nextProvider>,
   </StrictMode>,
 )
